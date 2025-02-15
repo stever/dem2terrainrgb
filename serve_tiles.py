56b82a89ef30321a67d464fa8a0e7b2beca18992
Steven Robertson <steverobertson@gmail.com>
2025-02-15 16:25:06 +0000

    Serving tiles

---
+++ serve_tiles.py
@@ -0,0 +1,17 @@
+from http.server import HTTPServer, SimpleHTTPRequestHandler
+import os
+import re
+
+class TileRequestHandler(SimpleHTTPRequestHandler):
+    def end_headers(self):
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.send_header('Access-Control-Allow-Methods', 'GET')
+        self.send_header('Content-Type', 'image/png')
+        super().end_headers()
+
+if __name__ == '__main__':
+    os.chdir('output/tiles')  # Move into tiles directory
+    httpd = HTTPServer(('localhost', 3002), TileRequestHandler)
+    print("Serving tiles at http://localhost:3002")
+    httpd.serve_forever()
+
